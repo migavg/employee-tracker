69ec523189f8943c0e359ce197d16be8d4dcb852
mvill <119754846+migavg@users.noreply.github.com>
2023-03-18 17:46:48 -0700

    created a schema and started adding to seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Dwight", "Schrute"),
+        ("Stanley", "Hudson"),
+        ("Jim", "Halpert"),
+        ("Andy", "Bernar"),
+        ("Angela", "Martin"),
+        ("Oscar", "Martinez"),
