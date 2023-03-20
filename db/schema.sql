071f1d58a39fde336b5c2fd52d2dbdfb40a5a690
mvill <119754846+migavg@users.noreply.github.com>
2023-03-19 21:55:38 -0700

    pluralized the words name and role just in case issues happen down the line since they were highlighted blue, also added more column date to seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,16 @@ USE employees_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    names VARCHAR(30) NOT NULL
  );
 
- CREATE TABLE role (
+ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    departmen_id INT NOT NULL 
+    departmen_id INT NOT NULL
+    FOREIGN KEY (department)
+    REFERENCES department(id)
     
  );
 
@@ -21,6 +23,8 @@ CREATE TABLE department (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
+    FOREIGN KEY (role)
+    REFERENCES role(id)
     manager_id INT
-    
+
  );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,23 @@
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Dwight", "Schrute"),
-        ("Stanley", "Hudson"),
-        ("Jim", "Halpert"),
-        ("Andy", "Bernar"),
-        ("Angela", "Martin"),
-        ("Oscar", "Martinez"),
+INSERT INTO department (names)
+VALUES ("Sales Team"),
+        ("Accounting Department"),
+        ("Warehousing");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Person", 50,000, 1),
+        ("Accountant", 55,000, 2),
+        ("Warehouse Associate", 35,000, 3)
+
+
+
+INSERT INTO employee (first_name, last_name, roles, manager_id)
+VALUES ("Dwight", "Schrute", 1),
+        ("Stanley", "Hudson", 1),
+        ("Jim", "Halpert", 1),
+        ("Andy", "Bernard", 1),
+        ("Angela", "Martin", 2),
+        ("Oscar", "Martinez", 2),
+        ("Kevin", "Malone", 2),
+        ("Lonny", "Collins", 3),
+        ("Darryl", "Philbin", 3)
+        ("Hidetoshi", "Hasagawa", 3)
