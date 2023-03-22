f2f4c52ad2ec68148fcbc26520d4556958e9b027
mvill <119754846+migavg@users.noreply.github.com>
2023-03-21 23:08:30 -0700

    added start prompt function throug inuqirer and added initial questions

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const mysql = require("mysql2");
+
+const questions = [
+
+    {
+        type:"list",
+        name:"commands",
+        message:"What would you like to do?",
+        choices: 
+        ["View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department"]
+    }
+]
+
+function startPrompt() {
+    inquirer.prompt(questions)
+    .then()
+
+}
+
+startPrompt();
