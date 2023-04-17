d8d95725a6ffd20c59bc2c0fd3a9ad40fc5126cb
mvill <119754846+migavg@users.noreply.github.com>
2023-04-16 20:46:03 -0700

    updated some seeds and schema text

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
  );
 
@@ -19,13 +19,11 @@ CREATE TABLE department (
 
  CREATE TABLE employee (
     id INT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES roles(id),
     manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 
  );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,25 @@
-INSERT INTO department (department_name)
-VALUES ("Sales Team"),
-        ("Accounting Department"),
-        ("Warehousing");
+INSERT INTO department (id, department_name)
+VALUES (1, "Sales Team"),
+        (2,"Accounting Department"),
+        (3, "Warehousing");
 
-INSERT INTO roles (title, salary, department_id)
-VALUES ("Sales Person", 50,000, 1),
-        ("Accountant", 55,000, 2),
-        ("Warehouse Associate", 35,000, 3)
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (1, "Sales Person", 50,000, 1),
+        (2, "Accountant", 55,000, 2),
+        (3, "Warehouse Associate", 35,000, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  (11, "Michael", "Scott", 1, NULL);
 
 
-
-INSERT INTO employee (first_name, last_name, roles, manager_id)
-VALUES ("Dwight", "Schrute", 1, NULL),
-        ("Stanley", "Hudson", 1, NULL),
-        ("Jim", "Halpert", 1, NULL),
-        ("Andy", "Bernard", 1, NULL),
-        ("Angela", "Martin", 2, NULL),
-        ("Oscar", "Martinez", 2, NULL),
-        ("Kevin", "Malone", 2, NULL),
-        ("Lonny", "Collins", 3, NULL),
-        ("Darryl", "Philbin", 3, NULL)
-        ("Hidetoshi", "Hasagawa", 3, NULL)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  (1, "Dwight", "Schrute", 1, 11),
+        (2, "Stanley", "Hudson", 1, 11),
+        (3, "Jim", "Halpert", 1, 11),
+        (4, "Andy", "Bernard", 1, 11);
+        (5, "Angela", "Martin", 2, NULL),
+        (6, "Oscar", "Martinez", 2, NULL),
+        (7, "Kevin", "Malone", 2, NULL),
+        (8, "Lonny", "Collins", 3, NULL),
+        (9, "Darryl", "Philbin", 3, NULL),
+        (10, "Hidetoshi", "Hasagawa", 3, NULL);
