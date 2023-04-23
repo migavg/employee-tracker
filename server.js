fe8827ac3ca04956cd67457825f1348a1c250c18
mvill <119754846+migavg@users.noreply.github.com>
2023-04-22 23:53:39 -0700

    pseudo coded some basic functions for each command to later place logic to view sql table info

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const consoleTable = require("console.table");
 
 const database = mysql.createConnection(
     {
-        host:"localhost",
-        user:"root",
-        password:"Bootcamp2023!",
+        host: "localhost",
+        user: "root",
+        password: "Bootcamp2023!",
         database: "employees_db"
 
     },
@@ -19,17 +19,17 @@ const database = mysql.createConnection(
 const questions = [
 
     {
-        type:"list",
-        name:"commands",
-        message:"What would you like to do?",
-        choices: 
-        ["View All Employees",
-        "Add Employee",
-        "Update Employee Role",
-        "View All Roles",
-        "Add Role",
-        "View All Departments",
-        "Add Department"]
+        type: "list",
+        name: "commands",
+        message: "What would you like to do?",
+        choices:
+            ["View All Employees",
+                "Add Employee",
+                "Update Employee Role",
+                "View All Roles",
+                "Add Role",
+                "View All Departments",
+                "Add Department"]
     }
 ]
 
@@ -37,21 +37,73 @@ const questions = [
 function startPrompt() {
     inquirer.prompt(questions)
 
-.then(function(answers){
-            console.table(answers);
-    const {commands} = answers;
+        .then(function (answers) {
+            // views commands used for debugging 
+            console.log(answers);
+            const { commands } = answers;
 
-    if ( commands === "View All Employees") {
-       viewEmployee();
-      };
-    });
+            if (commands === "View All Employees") {
+                viewEmployee();
+            }
+            if (commands === "Add Employee") {
+                addEmployee();
+            }
+            if (commands === "Update Employee Role") {
+                updateEmployee();
+            }
+            if (commands === "View All Roles") {
+                viewRoles();
+            }
+            if (commands === "Add Role") {
+                addRoles();
+            }
+            if (commands === "View All Departments") {
+                viewDepartments();
+            }
+            if (commands === "Add Department") {
+                addDepartments();
+            }
+
+
+
+        });
 };
 
 
 function viewEmployee() {
-    console.log("Here are Employees");
+    console.log("Here are Employees \n");
+    startPrompt();
 };
 
+function addEmployee() {
+    console.log("Add Employee Here \n");
+    startPrompt();
+};
+
+function updateEmployee() {
+    console.log("Updates to Employee Made Here \n");
+    startPrompt();
+};
+
+function addRoles() {
+    console.log("Add Roles Here \n");
+    startPrompt();
+};
+
+function viewRoles() {
+    console.log("Here you can view all Roles \n");
+    startPrompt();
+};
+
+function viewDepartments() {
+    console.log("Here are all the Departments \n");
+    startPrompt();
+};
+
+function addDepartments() {
+    console.log("Add Department Here \n");
+    startPrompt();
+};
 
 startPrompt();
 
